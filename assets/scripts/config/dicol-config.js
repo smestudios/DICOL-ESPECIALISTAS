5f2a06807efc03f3f866967bca3b3b951a4393d7
smestudios <smestudios0@gmail.com>
2026-09-24 17:34:52 -0500

    Configure legalizaciones Apps Script endpoint

--- assets/scripts/config/dicol-config.js
+++ assets/scripts/config/dicol-config.js
@@ -4,4 +4,7 @@ export const DICOL_CONFIG = Object.freeze({
   appsScriptUrl:
     "https://script.google.com/macros/s/AKfycbyxEKQfHQ_39AcIjS69B-5xRyleIsL4w25LJTGMmwyKMgp9uLucsNWFfHwuyWBOtUjVjQ/exec",
   apiVersion: "1.2",
+  // URL /exec del proyecto independiente "LEGALIZACIONES DE GASTOS".
+  legalizacionesAppsScriptUrl:
+    "https://script.google.com/macros/s/AKfycbylwmnOpqTWr08h9KzUppQL_kAAlFSPVeXQxbrB-L2SY3t_F9xP-iWBIqmAuXYXF9Dc3g/exec",
 });
